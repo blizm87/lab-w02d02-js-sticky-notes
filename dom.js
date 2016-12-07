25ef0024770af29c8463dc36c3403fdd96eb180a
Justin Samuels <blizm87@gmail.com>
2016-12-06 22:04:49 -0800

    gave functionality to button and counter.

--- dom.js
+++ dom.js
@@ -1,1 +1,24 @@
  // JavaScript goes here
+var addText = function (){
+  var newContent = document.createTextNode('p');
+  sNote.appendChild(newContent);
+}
+
+var createNote = function(){
+  if(event.target.value === 'Add a new note'){
+    sNote = document.createElement('div');
+    sNote.setAttribute('class','sNote');
+    main.appendChild(sNote);
+    input.value = document.querySelectorAll('.sNote').length;
+    sNote.addEventListener('click',addText);
+
+  }
+}
+
+var sNote;
+var input = document.querySelector('input');
+var main = document.querySelector('#main');
+var form = document.querySelector('form');
+form.addEventListener('click', createNote);
+
+

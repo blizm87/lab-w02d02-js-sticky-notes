558ceff776c9d60b13101c5b71f31bfa504289ef
Justin Samuels <blizm87@gmail.com>
2016-12-06 23:12:01 -0800

    truncated code and re-worded variables

--- dom.js
+++ dom.js
@@ -1,22 +1,23 @@
  // JavaScript goes here
 var addText = function (){
-  var newContent = document.createTextNode('p');
+  var sNoteText = window.prompt('Enter your text here:');
+  var newContent = document.createTextNode(sNoteText);
   sNote.appendChild(newContent);
 }
 
 var createNote = function(){
-  if(event.target.value === 'Add a new note'){
+  if(noteButton.value === 'Add a new note'){
     sNote = document.createElement('div');
     sNote.setAttribute('class','sNote');
     main.appendChild(sNote);
-    input.value = document.querySelectorAll('.sNote').length;
+    noteCounter.value = document.querySelectorAll('.sNote').length;
     sNote.addEventListener('click',addText);
 
   }
 }
-
 var sNote;
-var input = document.querySelector('input');
+var noteCounter = document.querySelector('input');
+var noteButton = document.querySelectorAll('input')[1];
 var main = document.querySelector('#main');
 var form = document.querySelector('form');
 form.addEventListener('click', createNote);
